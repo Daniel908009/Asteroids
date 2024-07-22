c037c279b298df29bfb5245d5f846dba847cf5da
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-22 15:06:35 +0200

    ship can now rotate, however there are still bugs
    also I tried to rotate the asteroids, but my attempt was unsuccesfull

--- main.py
+++ main.py
@@ -15,11 +15,19 @@ class Player(pygame.sprite.Sprite):
         self.horizontal_speed = 0
         self.direction = ""
         self.angle = 0
+        self.per_angle = 0
         self.image = image
         self.size = size
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
     def draw(self):
+        # rotating the player image according to the angle
+        self.image = pygame.transform.rotate(self.image, self.per_angle)
+        self.per_angle = 0
+
+        print(self.x, self.y)
+        
+        # drawing the player
         screen.blit(self.image, self.rect)
 
     def change_direction(self, direction):
@@ -45,7 +53,7 @@ class Bullet(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-        self.angle = angle - 90
+        self.angle = angle + 180
         self.image = image
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
@@ -53,29 +61,35 @@ class Bullet(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
     def move(self, speed):
-        self.x += math.cos(math.radians(self.angle)) * speed
-        self.y += math.sin(math.radians(self.angle)) * speed
+        #print(self.angle)
+        self.x += math.sin(math.radians(self.angle)) * speed
+        self.y += math.cos(math.radians(self.angle)) * speed
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
 # asteroid class
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.x = random.randint(0, 800)# currently they spawn inside the screen, but I will make them spaw outside the screen later on
-        self.y = random.randint(0, 600)# currently they spawn inside the screen, but I will make them spaw outside the screen later on
+        self.spawn_point = random.choice(spawn_points)
+        #print(self.spawn_point)
+        self.x = self.spawn_point[0]
+        self.y = self.spawn_point[1]
+        self.x = self.x
+        self.y = self.y
         self.angle = random.randint(0, 360)
-        self.rotation_speed = random.randint(-3, 3)
+        self.rotation_speed = random.randint(-3,3)
         self.image = random.choice(resized_asteroid_images)
         self.rect = self.image.get_rect(center=(self.x, self.y))
     
     def rotate(self):
         self.angle += self.rotation_speed
         self.image = pygame.transform.rotate(self.image, self.angle)
-        self.rect = self.image.get_rect(center=(self.x, self))
+        if self.angle >= 360 or self.angle <= -360:
+            self.angle = 0
+        self.rect = self.image.get_rect(center=(self.x, self.y))
         print(self.angle)
 
     def draw(self):
-        player.rotate()
         screen.blit(self.image, self.rect)
 
     def move(self, speed):
@@ -101,14 +115,53 @@ class Ufo(pygame.sprite.Sprite):
         pass
 
 
+# Functions
+
+# function to rotate the asteroids
+def rotation():
+    while running:
+        for asteroid in asteroids:
+            asteroid.rotate()
+        clock.tick(120)
+
+# function to handle the asteroids
+def asteroid_loop():
+    #rotate_thread = threading.Thread(target=rotation)
+    while running:
+    #    if not rotate_thread.is_alive():
+    #        rotate_thread.start()
+        # checking if the asteroids have hit the outer walls + 300 pixels of the screen
+        for asteroid in asteroids:
+            if asteroid.x <= 0 - 300 + asteroid.image.get_width() / 2 or asteroid.x >= 800 + 300 - asteroid.image.get_width() / 2 or asteroid.y <= 0 - 300 + asteroid.image.get_height() / 2 or asteroid.y >= 600 + 300 - asteroid.image.get_height() / 2:
+                asteroids.remove(asteroid)
+    
+        # moving the asteroids
+        for asteroid in asteroids:
+            asteroid.move(random.randint(1, 3))
+
+        clock.tick(60)
+
+
 # initialize pygame
 pygame.init()
 # setting up the screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 # setting up the title and icon
 pygame.display.set_caption("Asteroids")
 icon = pygame.image.load("asteroid1.png")
 pygame.display.set_icon(icon)
+
+# setting up all the possible spawn points outside the screen
+spawn_points = []
+for i in range(200//10):
+    for j in range(600//10):
+        spawn_points.append((800+i*10, j*10))
+        spawn_points.append((-800+i*10, j*10))
+        spawn_points.append((i*10, 600+j*10))
+        spawn_points.append((i*10, -600+j*10))
+
+print("spawn points set up")
+
 
 # creating the player
 player_image = pygame.image.load("spaceship.png")
@@ -133,6 +186,9 @@ for asteroid_image in asteroid_images:
 # creating the asteroids group
 asteroids = pygame.sprite.Group()
 
+# setting up a thread for asteroids
+asteroid_thread = threading.Thread(target=asteroid_loop)
+
 # setting up the ufos
 ufo_image = pygame.image.load("ufo.png")
 resized_ufo_image = pygame.transform.scale(ufo_image, (64, 64))
@@ -144,6 +200,10 @@ ufos = pygame.sprite.Group()
 running = True
 clock = pygame.time.Clock()
 while running:
+
+    # starting the asteroid thread
+    if not asteroid_thread.is_alive():
+        asteroid_thread.start()
 
     # setting the background color
     screen.fill((0, 0, 0))
@@ -172,12 +232,16 @@ while running:
                 ufos.add(ufo)
             elif event.key == pygame.K_q:
                 print("q key pressed")
-                player.angle += 90
+                player.angle += 45
+                player.per_angle += 45
+                print(player.angle)
+                print(player.per_angle)
                 if player.angle == 360 or player.angle == -360:
                     player.angle = 0
             elif event.key == pygame.K_e:
                 print("e key pressed")
-                player.angle -= 90
+                player.angle -= 45
+                player.per_angle -= 45
                 if player.angle == 360 or player.angle == -360:
                     player.angle = 0
 
@@ -187,10 +251,6 @@ while running:
     # moving the bullets
     for bullet in bullets:
         bullet.move(8)
-    
-    # moving the asteroids
-    for asteroid in asteroids:
-        asteroid.move(random.randint(1, 3))
 
     # moving the ufos
     for ufo in ufos:
@@ -216,11 +276,6 @@ while running:
         if bullet.x <= 0 + bullet.image.get_width() / 2 or bullet.x >= 800 - bullet.image.get_width() / 2 or bullet.y <= 0 + bullet.image.get_height() / 2 or bullet.y >= 600 - bullet.image.get_height() / 2:
             bullets.remove(bullet)
 
-    # checking if the asteroids have hit the outer walls of the screen
-    for asteroid in asteroids:
-        if asteroid.x <= 0 + asteroid.image.get_width() / 2 or asteroid.x >= 800 - asteroid.image.get_width() / 2 or asteroid.y <= 0 + asteroid.image.get_height() / 2 or asteroid.y >= 600 - asteroid.image.get_height() / 2:
-            asteroids.remove(asteroid)
-
     # checking if the bullets have hit the asteroids or the ufos
     for bullet in bullets:
         for asteroid in asteroids:
@@ -232,7 +287,7 @@ while running:
                 bullets.remove(bullet)
                 ufos.remove(ufo)
     
-    print(player.angle)
+    #print(player.angle)
 
     # drawing the player
     player.draw()
@@ -240,8 +295,9 @@ while running:
     # drawing the bullets
     bullets.draw(screen)
 
-    # drawing the asteroids
+    # drawing the asteroids, asteroids have to be drawn in the main loop, to keep them sync
     asteroids.draw(screen)
+    #print(len(asteroids))
 
     # drawing the ufos
     ufos.draw(screen)
